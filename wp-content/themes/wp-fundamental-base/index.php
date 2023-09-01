c4219c49bbd05d61d8e02082349311911aa2eaee
Omar Ochoa <omar@ochoawebdesign.com>
2023-08-31 19:11:20 -0700

    Nit: index.php code indent

--- wp-content/themes/wp-fundamental-base/index.php
+++ wp-content/themes/wp-fundamental-base/index.php
@@ -1,12 +1,12 @@
 <?php get_header(); ?>
 
 <body class="<?php body_class(); ?>">
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-    <h1><?php the_title(); ?></h1>
-    <?php the_content(); ?> 
-<?php endwhile; else : ?>
-	<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
-<?php endif; ?>
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <h1><?php the_title(); ?></h1>
+        <?php the_content(); ?> 
+    <?php endwhile; else : ?>
+        <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+    <?php endif; ?>
 </body>
 
 <?php get_footer(); ?>
